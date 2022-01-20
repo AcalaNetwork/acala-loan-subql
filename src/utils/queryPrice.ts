6db4a8c2596d2429df67aad4ec2f7d7af5214a43
shenger <714275326@qq.com>
2022-01-20 14:47:59 +0800

    fix

--- src/utils/queryPrice.ts
+++ src/utils/queryPrice.ts
@@ -2,18 +2,33 @@ import { FixedPointNumber as FN, forceToCurrencyIdName } from "@acala-network/sd
 import { Option } from "@polkadot/types";
 import { TimestampedValue } from '@open-web3/orml-types/interfaces';
 
+const queryTotalStaking = async () => {
+  if (api.query.homa) {
+    const stakingLedgers = await api.query.homa.stakingLedgers.entries();
+    const totalInSubAccount = stakingLedgers.reduce((acc, cur) => {
+      const item = (cur[1].toJSON() as any).bonded.toString();
+      return acc.add(FN.fromInner(item, 12));
+    }, new FN(0, 12));
+    logger.info(totalInSubAccount.toString())
+    const bonds = await api.query.homa.toBondPool()
+    return totalInSubAccount.add(FN.fromInner(bonds.toString(), 12));
+  } else {
+    const total = await api.query.homaLite.totalStakingCurrency();
+    return FN.fromInner(total.toString(), 12);
+  }
+}
+
 export const queryPrice = async (currency: string): Promise<FN> => {
   const liquidCurrencyId = 'LKSM';
   const stakingCurrencyId = 'KSM';
 
   if (liquidCurrencyId && forceToCurrencyIdName(liquidCurrencyId) === forceToCurrencyIdName(currency)) {
-    const [_stakingTokenPrice, _stakingBalance, _liquidIssuance] = await Promise.all([
+    const [_stakingTokenPrice, _liquidIssuance] = await Promise.all([
       api.query.acalaOracle.values({ Token: forceToCurrencyIdName(stakingCurrencyId) }),
-      api.query.homaLite.totalStakingCurrency(),
       api.query.tokens.totalIssuance({ Token: currency })
     ])
     const stakingTokenPrice = (_stakingTokenPrice as Option<TimestampedValue>).unwrapOrDefault().value.toString();
-    const stakingBalance = FN.fromInner(_stakingBalance.toString(), 12);
+    const stakingBalance = await queryTotalStaking();
     const liquidIssuance = FN.fromInner(_liquidIssuance.toString(), 12);
     const ratio = liquidIssuance.isZero() ? FN.ZERO : stakingBalance.div(liquidIssuance);
     return FN.fromInner(stakingTokenPrice, 18).times(ratio);
